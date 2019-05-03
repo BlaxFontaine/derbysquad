572e2f6ca0a62a3af8ad41790e4a03860c38d4eb
Valerie Gehle <valerie.gehle@gmail.com>
2019-05-03 17:59:05 +0200

    show of a league

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -2,4 +2,9 @@ class LeaguesController < ApplicationController
   def index
     @leagues = League.all
   end
+
+  def show
+    @league = League.find(params[:id])
+    @teams = Team.where(league_id: params[:id])
+  end
 end
