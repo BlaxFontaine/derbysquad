3c6a880972e603222e57790b2b44fe767ebc888a
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-20 17:21:18 +0100

    api routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+  get "leagues/:id", to: 'pages#home'
+  get "leagues/new", to: 'pages#home'
+  # API routing
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :leagues, only: [ :index, :show, :create ]
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :leagues do
     resources :teams
