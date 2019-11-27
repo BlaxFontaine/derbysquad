d419d3105292c7dadeccc3f11a7d38d95aa529d0
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-27 18:27:10 +0100

    change font color

--- app/javascript/rep/components/league.jsx
+++ app/javascript/rep/components/league.jsx
@@ -12,6 +12,7 @@ class League extends Component {
           <img src={this.props.url.concat(this.props.league.logo)} />
           <div className="card-product-infos">
             <h2>{this.props.league.name}</h2>
+
             <p>{this.props.league.city}, {this.props.league.country}</p>
           </div>
           <a className="card-link" href="#" onClick={this.handleClick}></a>
