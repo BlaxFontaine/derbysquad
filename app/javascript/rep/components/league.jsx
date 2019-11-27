6a7c7f55a7006c71bc985b78d33203ff399051cf
Blax Fontaine <valerie.gehle@gmail.com>
2019-11-27 18:25:22 +0100

    league card front

--- app/javascript/rep/components/league.jsx
+++ app/javascript/rep/components/league.jsx
@@ -8,15 +8,13 @@ class League extends Component {
 
   render() {
     return (
-      <div className={`card${this.props.selected ? ' active' : ''}`}>
-        <div className="card-product">
+      <div className={`card-product${this.props.selected ? ' active' : ''}`}>
           <img src={this.props.url.concat(this.props.league.logo)} />
           <div className="card-product-infos">
             <h2>{this.props.league.name}</h2>
             <p>{this.props.league.city}, {this.props.league.country}</p>
           </div>
           <a className="card-link" href="#" onClick={this.handleClick}></a>
-        </div>
       </div>
     );
   }
