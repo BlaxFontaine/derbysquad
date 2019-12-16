5ee2ee1ec4a4f4e5ad3131240534def79544ca94
Blax Fontaine <valerie.gehle@gmail.com>
2019-12-16 22:57:23 +0100

    added missing npm packages react and react-dom

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,7 +1,8 @@
 import "bootstrap";
-import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+// import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
+// import "../rep"
 
-import { initMapbox } from '../plugins/init_mapbox';
+// import { initMapbox } from '../plugins/init_mapbox';
 
-initMapbox();
+// initMapbox();
 
